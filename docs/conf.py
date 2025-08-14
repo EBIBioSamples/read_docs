75e265ba29b5fc9214555b422bff1af27cb5bd9d
Wei Kheng Teh <68549377+Wkt8@users.noreply.github.com>
2025-08-14 10:49:33 +0100

    add static folder and move custom.css

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,10 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_logo = 'BioSamples_logo.png'
-html_css_files = ["custom.css"]
+html_static_path = ['_static']
+
+def setup(app):
+    app.add_css_file('custom.css')
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
